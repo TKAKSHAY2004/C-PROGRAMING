e464410b4057d740f2d021ba0641104ad73aec53
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-07 22:48:55 +0530

    to calculate Area of right angled triangle

--- CHAP_45.c
+++ CHAP_45.c
@@ -7,7 +7,7 @@ int main()
     scanf("%f", &perpendicular);
     
     area =(base * perpendicular)/2;
-    printf("Area of triangle : %0.2f\n", area);
+    printf("Area of right angled triangle : %0.2f\n", area);
      
    
     return 0;
