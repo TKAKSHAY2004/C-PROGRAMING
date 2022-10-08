c6f092f67f080a92a6fbd4922ff290416131a92a
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-08 23:51:17 +0530

    surface area of cube

---
+++ CHAP_82.c
@@ -0,0 +1,9 @@
+void main()
+{
+    float side, surfacearea;
+ 
+    printf("Enter the length of a side=");
+    scanf("%f", &side);
+    surfacearea = 6.0 * side * side;
+    printf("Surface area of cube = %6.2f \n", surfacearea);
+}
