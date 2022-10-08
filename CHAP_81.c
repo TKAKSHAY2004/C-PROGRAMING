69ecfe51e21d39ff40013ea784f671e81296e864
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-08 23:46:06 +0530

    Volume of cube

---
+++ CHAP_81.c
@@ -0,0 +1,10 @@
+int main()
+{
+    float side, volume;
+ 
+    printf("Enter the length of a side=");
+    scanf("%f", &side);
+    volume =  side*side*side;
+    
+    printf("  Volume of cube = %.2f \n",volume);
+}
