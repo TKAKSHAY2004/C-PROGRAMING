12f6323f3b367ba6e2c07ccf0d7cd80ebd48bff6
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 20:43:02 +0530

    Pre increment operators

--- PROGRAM__29.c
+++ PROGRAM__29.c
@@ -1,6 +1,6 @@
 int main()
 {
   int a=94;
-  printf("The value of a returned after post increment is = %d",a++);
+  printf("The value of a returned after pre increment is = %d",++a);
   printf("\nThe value of a now is = %d",a);
 }
