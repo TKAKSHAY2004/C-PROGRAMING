9d24554e544d2fa592af2b541b9a4cf7b3f1e2d8
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 20:36:21 +0530

    Increment and Decrement operator

---
+++ PROGRAM__25.c
@@ -0,0 +1,11 @@
+int main()  
+{ 
+int a = 9, b = 99;  
+float c = 8.5, d = 98.5;
+printf("RA2211042010057\n");
+printf("++a = %d \n", ++a);  
+printf("--b = %d \n", --b);  
+printf("++c = %f \n", ++c); 
+printf("--d = %f \n", --d); 
+return 0; 
+}
