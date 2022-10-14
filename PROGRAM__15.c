da6072d652274986765c4fa6b69a3f18297cfc18
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 20:03:35 +0530

    To find square of a number

--- PROGRAM__15.c
+++ PROGRAM__15.c
@@ -1,13 +1,12 @@
 int main()
 {
   int number, Square;
- 
+  printf("RA2211042010057\n");
   printf(" \n Please Enter any integer Value : ");
   scanf("%d", &number);
   
   Square = number * number;
   
   printf("\n Square of a given number %d is  =  %d", number, Square);
- 
   return 0;
 }
