4f63a6b4d13d17c7dc71c5a992cc1afec82c36e9
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 20:04:57 +0530

    To find square root of a number

--- PROGRAM__16.c
+++ PROGRAM__16.c
@@ -1,11 +1,10 @@
 
 int main() {
    double number, squareRoot;
-
+   printf("RA2211042010057\n");
    printf("Enter a number: ");
    scanf("%lf", &number);
 
-   // computing the square root
    squareRoot = sqrt(number);
 
    printf("Square root of %.2lf =  %.2lf", number, squareRoot);
