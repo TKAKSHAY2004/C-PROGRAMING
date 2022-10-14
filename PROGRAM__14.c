36e8b24638b8ec6ea5350b70bdaa43be0d5fb694
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 20:01:51 +0530

    To count number of digits in an integers

--- PROGRAM__14.c
+++ PROGRAM__14.c
@@ -1,15 +1,13 @@
 int main() {
   long number, temp;
   int count = 0;
+  printf("RA2211042010057\n");
   printf("Enter an integer: ");
   scanf("%ld", &number);
-
   temp = number;
-  
   do {
     temp /= 10;
     ++count;
   } while (temp != 0);
-
   printf("Number of digits in the number %ld = %d", number,count);
 }
