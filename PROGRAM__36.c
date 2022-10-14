154b04eb31ce0c03630f1d3d5dc66f2cd0780a85
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 21:13:43 +0530

    To find circumference of circle

---
+++ PROGRAM__36.c
@@ -0,0 +1,14 @@
+int main()
+{
+  float Pi=3.14, cricumference, radius;
+  printf("RA2211042010057\n");
+  printf("Enter radius of circle: ");
+  
+  scanf("%f",&radius);
+
+  circumference = 2 * Pi * radius; 
+  printf("Circumference of circle is: %.3f\n",circumference);
+
+
+  return 0;
+}
