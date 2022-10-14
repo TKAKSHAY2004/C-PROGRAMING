1403c06de856961994a027845ffe5a665d792620
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-15 00:00:53 +0530

    To solve given problem

---
+++ PROGRAM__73.c
@@ -0,0 +1,11 @@
+int main()  
+{ 
+int a = 10, b = 100;  
+float c = 10.5, d = 100.5; 
+printf("RA2211042010057\n");
+printf("++a = %d \n", ++a);  
+printf("--b = %d \n", --b);  
+printf("++c = %f \n", ++c); 
+printf("--d = %f \n", --d); 
+return 0; 
+}
