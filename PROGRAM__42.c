9c866304d033f627cc75f06aeb7eb168413796af
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 21:46:06 +0530

    To find surface area of cube

--- PROGRAM__42.c
+++ PROGRAM__42.c
@@ -1,7 +1,7 @@
 void main()
 {
     float side, surfacearea;
- 
+    printf("RA2211042010057\n");
     printf("Enter the length of a side=");
     scanf("%f", &side);
     surfacearea = 6.0 * side * side;
