05fcf6c8d7a7738e84fa04ac7a22bc3038ac5b2b
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 22:45:08 +0530

    Convert week to days

---
+++ PROGRAM__61.c
@@ -0,0 +1,13 @@
+int main()  
+{  
+    int  week,days;  
+    printf("RA2211042010057\n");
+    printf("Enter number of weeks:");  
+    scanf("%d", &week);  
+  
+    days=week*7;  
+  
+    printf("Enter number of days %d\n",days);  
+  
+    return 0;  
+}  
