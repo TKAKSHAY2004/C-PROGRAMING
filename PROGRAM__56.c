e13c823539d805c3b2315f9288606327b6745eff
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 22:34:29 +0530

    Convert foot to yard

---
+++ PROGRAM__56.c
@@ -0,0 +1,13 @@
+int main()  
+{  
+    float  yard,foot;  
+    printf("RA2211042010057\n");
+    printf("Enter value of foot:");  
+    scanf("%f", &foot);  
+  
+    yard=foot*3.00;  
+  
+    printf("value in yard is %.2f\n",yard);  
+  
+    return 0;  
+}  
