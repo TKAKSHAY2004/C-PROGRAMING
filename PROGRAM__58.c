caac07024dd7a5d3a71847f1b9f6928b202e0e91
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 22:48:21 +0530

    Convert decade to days

---
+++ PROGRAM__58.c
@@ -0,0 +1,13 @@
+int main()  
+{  
+    int  decade,days;  
+    printf("RA2211042010057\n");
+    printf("Enter number of decade:");  
+    scanf("%d", &decade);  
+  
+    days=decade*3650;  
+  
+    printf("Enter number of days %d\n",days);  
+  
+    return 0;  
+}  
