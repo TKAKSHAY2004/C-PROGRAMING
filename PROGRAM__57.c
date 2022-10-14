0cbe7cbcfd17d255465d8870e4cecf1d96cf6fb8
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 22:30:39 +0530

    Convert foot to inches

---
+++ PROGRAM__57.c
@@ -0,0 +1,13 @@
+int main()  
+{  
+    float  inch,foot;  
+    printf("RA2211042010057\n");
+    printf("Enter value of foot:");  
+    scanf("%f", &foot);  
+  
+    inch=foot/12.00;  
+  
+    printf("value in inch is %.2f\n",inch);  
+  
+    return 0;  
+}  
