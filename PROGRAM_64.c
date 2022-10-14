dd409956b3933b503001c78027ec1b5b30145176
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 22:55:41 +0530

    Reversing the number using with while loop

--- PROGRAM_64.c
+++ PROGRAM_64.c
@@ -1,14 +1,15 @@
 int main()    
 {    
-int n, reverse=0, rem;    
+int n, reverse=0, rem; 
+printf("RA2211042010057\n");
 printf("Enter a number: ");    
   scanf("%d", &n);    
   while(n!=0)    
-  {    
+     
      rem=n%10;    
      reverse=reverse*10+rem;    
      n/=10;    
-  }    
-  printf("Reversed Number: %d",reverse);    
+     
+printf("Reversed Number: %d",reverse);    
 return 0;  
 }   
