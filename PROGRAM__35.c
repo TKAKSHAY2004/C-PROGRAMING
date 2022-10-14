f00cf4c1a1954e5a520d5f94a74ac382e9915494
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 21:02:15 +0530

    To find area of parallelogram

--- PROGRAM__35.c
+++ PROGRAM__35.c
@@ -2,7 +2,7 @@ int main()
 {
   
   float height,base, Area;
-  
+  printf("RA2211042010057\n");
   printf("Enter the height of the parallelogram: ");
   scanf("%f",&height);
   printf("Enter the base of the parallelogram:");
