b552418cd48c99c19a4669e7fbadf8281c77b448
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 21:10:38 +0530

    To find area of circle

--- PROGRAM__37.c
+++ PROGRAM__37.c
@@ -9,8 +9,6 @@ int main()
   area = Pi * radius * radius;
   printf("Area of circle is: %.3f\n",area);
 
-  circumference = 2 * Pi * radius;
-  printf("Circumference of circle is: %.3f",circumference);
 
   return 0;
 }
