1dbda7339b552201b10af332361d4a09bf233c8b
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 21:42:35 +0530

    To find area of trapezium

--- PROGRAM__39.c
+++ PROGRAM__39.c
@@ -2,7 +2,7 @@ int main()
 {
     float a, b, h;
     float area;
- 
+    printf("RA2211042010057\n");
     printf("Enter the value for two bases & height of the trapezium: \n");
     scanf("%f%f%f", &a, &b, &h);
     area = 0.5 * (a + b) * h ;
