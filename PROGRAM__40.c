fc96f374594679cff6b7f2789687b631f59654fb
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-10-14 21:43:52 +0530

    To find area of right angled triangle

--- PROGRAM__40.c
+++ PROGRAM__40.c
@@ -1,6 +1,7 @@
 int main()
 {
     float base, perpendicular, area;
+    printf("RA2211042010057\n");
     printf("Enter base of right angled triangle=");
     scanf("%f", &base);
     printf("Enter perpendicular of right angled triangle=");
