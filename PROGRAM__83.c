c6aa0536e9451701a084c9547850327f0728074b
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-11-02 18:42:25 +0530

    Implicit Type Conversions

---
+++ PROGRAM__83.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+int i, intsum;
+printf("i = ");
+scanf("%d", &i);// read value of i
+char ch = 'a'; // assing character 'a' to ch
+float floatsum;
+intsum = ch + i; // add values of it and ch
+
+floatsum = ch + i; // add values i and chi 
+printf("Integer result %d, Float result = %f\n", intsum, floatsum );
+
+}
