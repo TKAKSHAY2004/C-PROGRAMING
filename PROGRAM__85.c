59589fba9800bb2357d9cc1065df7ad1c1bed280
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-11-03 17:13:49 +0530

    Getchar and Putchar

---
+++ PROGRAM__85.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter a character : ");
+    ch = getchar(); //use a function to read a character
+    printf("The entered character is : ");
+    putchar(ch);//use a function to print the character
+    return 0;
+}
