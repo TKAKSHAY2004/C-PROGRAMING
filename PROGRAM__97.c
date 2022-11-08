e23b1c567be9f5b9c0684a93b5093a51557482b2
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-11-08 09:36:56 +0530

    To check whether the number is divisible by 3 or not

---
+++ PROGRAM__97.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int number;
+  printf("Enter a number : ");
+  scanf("%d", &number);
+  
+  if(number % 3 == 0)
+    printf("The number is divisible by 3");
+  else 
+    printf("The number is not divisible by 3");
+  return 0;
+}
