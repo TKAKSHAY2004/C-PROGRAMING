568e7ed432aac6097c62344144258bf32dec4274
TKAKSHAY2004 <113873680+TKAKSHAY2004@users.noreply.github.com>
2022-11-08 08:33:17 +0530

    To find distance travelled by an object

---
+++ PROGRAME__89.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    float a,u,t,s;
+    printf("Enter the acceleration value : ");
+    scanf("%f",&a);
+    printf("Enter the initial velocity : ");
+    scanf("%f", &u);
+    printf("Enter the time taken : ");
+    scanf("%f",&t);
+     
+    s = u*t + 0.5*a*t*t;
+    
+    printf("Distance travelled : %.3f\n", s);
+
+    return 0;
+}
